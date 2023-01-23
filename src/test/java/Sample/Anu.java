8b86aa5824dbadb8020c394fc028f48654228e6d
Lenovo <Lenovo@DESKTOP-JTJ28NU>
2023-01-23 17:22:31 +0530

    updated

--- src/test/java/Sample/Anu.java
+++ src/test/java/Sample/Anu.java
@@ -1,5 +1,7 @@
 package Sample;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class Anu 
@@ -7,6 +9,7 @@ public class Anu
 	@Test(groups = "smoke")
 	public void m3()
 	{
+		WebDriver driver = new ChromeDriver();
 		System.out.println("Hii gud mrng");
 		String b = System.getProperty("browser");
 		System.out.println(b);
